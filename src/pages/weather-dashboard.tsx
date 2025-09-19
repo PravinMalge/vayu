2c9cb432df7cd51b0645f662cf2aafe7be7d01ba
Pravin Malge <pravinmalge123@gmail.com>
2025-09-19 18:01:32 +0530

    location

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,6 +1,8 @@
+import WeatherSkeleton from '@/components/loading-skeleton';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Button } from '@/components/ui/button'
 import { useGeolocation } from '@/hooks/use-geolocation'
-import { RefreshCcw } from 'lucide-react'
+import { CheckCircle2Icon, MapPin, RefreshCcw } from 'lucide-react'
 
 const WeatherDashboard = () => {
 
@@ -15,7 +17,43 @@ const handleRefresh = () => {
   }
 };
 if (locationLoading) {
-  console.error(locationError);
+  return <WeatherSkeleton />;
+}
+
+
+if (locationError) {
+  return (
+    <>
+      <Alert variant="destructive">
+        <CheckCircle2Icon />
+        <AlertDescription className='flex flex-col gap-4'>
+          <p>{locationError}</p>
+          <Button onClick={getLocation} variant={'outline'} className='w-fit'>
+            <MapPin className='mr-2 h-4 w-4' />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    </>
+  );
+}
+
+if (!coordinates) {
+  return (
+    <>
+      <Alert variant="destructive">
+        <AlertTitle>Location Required</AlertTitle>
+        <CheckCircle2Icon />
+        <AlertDescription className='flex flex-col gap-4'>
+          <p>Please enable location services to see your local weather.</p>
+          <Button onClick={getLocation} variant={'outline'} className='w-fit'>
+            <MapPin className='mr-2 h-4 w-4' />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    </>
+  );
 }
 
   return (
